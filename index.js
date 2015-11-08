63981f26286f928ab72b71544b5505a130b065c8
Matus Szabo <matus.szabo@gmail.com>
2015-11-08 16:42:01 +0100

    fixed: module references

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
     compare: require('./lib/compare.js'),
     group: require('./lib/group.js'),
-    sort: require('./lib/sort.js'),
     synchronize: require('./lib/synchronize.js')
 };
